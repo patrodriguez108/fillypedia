b1e7a0b1feb7862b3e82cae9264ac925606292b2
Baska Batbold <baskabatbold@gmail.com>
2017-11-01 15:32:15 -0500

    some article stuff for homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @recent_articles = Article.all.order('created_at DESC LIMIT 3')
+    @recent_articles = Article.where(is_published: true).order('created_at DESC').limit(3).pluck(:published_version_id)
     @categories = Category.all
   end
 
