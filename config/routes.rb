be5aea4c55273ba997adf02d028add152300ddf9
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-11-01 15:11:54 -0500

    Implement user login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
 
   get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
