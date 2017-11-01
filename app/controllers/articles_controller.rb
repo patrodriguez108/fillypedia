67bd829e27170433ac8c75ab44ee19ce71d33795
Victoria Luc <victoriajeniluc@gmail.com>
2017-11-01 17:36:47 -0500

    Create a new article and able to save version into the database with text editor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,24 +9,12 @@ class ArticlesController < ApplicationController
     @version = Version.new
   end
 
-  def create
-    @version = Version.new(version_params)
-    @version.author = current_user
-    if @version.save
-      redirect_to root_path, notice: "The article has been successfully created"
-    else
-      render "new"
-    end
-  end
+
 
   def show
     @article = Article.find(params[:id])
   end
 
-  private
-  def version_params
-    params.require(:version).permit(:title, :body)
-  end
 
 
 
