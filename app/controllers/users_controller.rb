97f4e4343f222896863ee171c5154895543bc293
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-11-01 14:43:44 -0500

    Add validations for username and email in User model and modify user controllers and view for user registration to accept username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,15 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to root
+      redirect_to '/'
     else
-      redirect_to new_user_path
+      redirect_to '/signup'
     end
   end
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :username, :email, { presence: true, uniqueness: true }
+
 end
