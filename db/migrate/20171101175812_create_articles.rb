351f2547fc24af9d0f66044058309f40aa356c9e
Matthew Black <matthewallenblack@gmail.com>
2017-11-01 14:11:06 -0500

    Add is_featured and default click_count to 0.

--- db/migrate/20171101175812_create_articles.rb
+++ db/migrate/20171101175812_create_articles.rb
@@ -3,8 +3,9 @@ class CreateArticles < ActiveRecord::Migration[5.1]
     create_table :articles do |t|
       t.boolean :is_published, default: false
       t.string :photo_url
-      t.integer :click_count
+      t.integer :click_count, default: 0
       t.integer :published_version_id
+      t.boolean :is_featured, default: false
 
       t.timestamps
     end
